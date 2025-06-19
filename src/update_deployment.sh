45158e4bd9903d905caf6b6e3f0262ddfcb9a5c3
champsing <champsingxiang@gmail.com>
2025-06-20 07:55:21 +0800

    set log level

--- src/update_deployment.sh
+++ src/update_deployment.sh
@@ -3,6 +3,5 @@ git pull
 python3 manage.py collectstatic --no-input
 song_lyric_handler/venv/bin/gunicorn \
   --bind 0.0.0.0:8000 \
-  --log-level debug \
-  --capture-output \
+  --log-level info \
   song_lyric_handler.wsgi:application
