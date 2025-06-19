dcef09e2cd900f96a4c2be7ed9d750b4b0a08151
champsing <champsingxiang@gmail.com>
2025-06-19 20:04:08 +0800

    make startup shell gitted

---
+++ src/update_deployment.sh
@@ -0,0 +1,4 @@
+git checkout master
+git pull
+python3 manage.py collectstatic --no-input
+song_lyric_handler/venv/bin/gunicorn --bind 0.0.0.0:8000 --log-level debug song_lyric_handler.wsgi:application
