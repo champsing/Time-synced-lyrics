67fc3d9c4c6e76d6821fcc143342070b719db717
champsing <champsingxiang@gmail.com>
2025-06-20 08:59:49 +0800

    use the latest uptime

--- src/utils/get_system_uptime.py
+++ src/utils/get_system_uptime.py
@@ -1,5 +1,5 @@
 def get_system_uptime():
     with open("/proc/uptime", "r") as f:
-        uptime_seconds = float(f.readline().split()[0])
+        uptime_seconds = float(f.readline().split()[-1])
 
     return uptime_seconds
