5abeff95f16d7eca2b579e1953d94f2c3d9bd4b3
champsing <champsingxiang@gmail.com>
2026-01-29 00:04:22 +0800

    add error handling on system info fetchers

--- src/utils/get_system_info.py
+++ src/utils/get_system_info.py
@@ -1,11 +1,16 @@
 def get_system_uptime():
-    with open("/proc/uptime", "r") as f:
-        uptime_seconds = float(f.readline().split()[-1])
+    try:
+        with open("/proc/uptime", "r") as f:
+            return float(f.readline().split()[-1])
+    except Exception as e:
+        print(f"Error reading system uptime: {str(e)}")
+        return 0.0
 
-    return uptime_seconds
 
 def get_version_number():
-    with open("/home/tsl/Time-synced-lyrics/src/version_number", "r") as f:
-        version_number = str(f.readline().strip())
-
-    return version_number
+    try:
+        with open("/home/tsl/Time-synced-lyrics/src/version_number", "r") as f:
+            return str(f.readline().strip())
+    except Exception as e:
+        print(f"Error reading version number: {str(e)}")
+        return "unknown"
