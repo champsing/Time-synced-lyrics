e78697af1efb1c5d9a7731d2ed7cec7332bb0e78
champsing <champsingxiang@gmail.com>
2026-01-30 06:50:32 +0800

    add new song mga lulu

---
+++ src/database/migration/017_add_mga_lulu.sql
@@ -0,0 +1,50 @@
+INSERT INTO
+    songs (
+        song_id,
+        available,
+        hidden,
+        folder,
+        art,
+        artist,
+        lyricist,
+        title,
+        subtitle,
+        album,
+        versions,
+        furigana,
+        translation,
+        updated_at,
+        lang,
+        credits
+    )
+VALUES
+    (
+        1867354081,
+        1,
+        NULL,
+        'Mrs. GREEN APPLE - lulu.',
+        'https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/c6/37/51/c6375129-a4a6-6fca-fd1b-bd02b788b58f/Joba6e2e2f2-78fa-4fc7-b378-6362ebfb8e21-213296121-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_417683161_2502934387-Time1767836143126.png/900x900bb.webp',
+        '96222103300',
+        '1360524149',
+        'lulu.',
+        NULL,
+        NULL,
+        '[{"version": "original", "id": "MjeiIal1ZR0", "default": true, "duration": "4:55"}]',
+        0,
+        '{"available": false, "author": "", "cite": ""}',
+        '2026-01-30',
+        'ja',
+        '
+        {
+            "performance": [
+                {"name": "暫無資料", "contribution": ["暫無資料"]}
+            ],
+            "song_writing": [
+                {"name": "暫無資料", "contribution": ["暫無資料"]}
+            ],
+            "engineering": [
+                {"name": "暫無資料", "contribution": ["暫無資料"]}
+            ]
+        }
+        '
+    )
